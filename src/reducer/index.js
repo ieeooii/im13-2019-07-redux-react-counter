ecae57c29db8160e3c20b32dd647bef0db90b06d
iee1020 <yl9536@gmail.com>
2019-07-05 20:28:05 +0900

    redux reducer

---
+++ src/reducer/index.js
@@ -0,0 +1,21 @@
+import { INCREMENT, DECREMENT, SET_DIFF } from "../actions";
+
+const initialState = {
+  value: 0,
+  diff: 0
+};
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return Object.assign({}, state, { value: state.value + state.diff });
+    case DECREMENT:
+      return Object.assign({}, state, { value: state.value - state.diff });
+    case SET_DIFF:
+      return Object.assign({}, state, { diff: action.diff });
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
