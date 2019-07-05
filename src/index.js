836ad5d6f4177677d3f76a7d45fa1ee8e0423f3b
iee1020 <yl9536@gmail.com>
2019-07-05 20:27:32 +0900

    index.js-root modify

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createStore } from "redux";
+import counterReducer from "./reducer";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(counterReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
